c75814e15e450db59bc3a85e63ac809848e26294
Adwait Thattey <coderdude1999@gmail.com>
2020-05-02 08:55:01 +0530

    django: init django project
    A new project is initialized with django 3

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tfexplorer.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TFExplorer.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
